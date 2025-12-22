1155fc7ea45a0552c2cf6b09e22924b214803afa
Samira Witschi <samiraaa15@hotmail.com>
2025-12-22 11:44:12 +0100

    Casino8 done

--- src/net/ictcampus/semodul/Game.java
+++ src/net/ictcampus/semodul/Game.java
@@ -3,32 +3,14 @@ package net.ictcampus.semodul;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Game {
+public abstract class Game {
     private String name;
 
     public Game(String name) {
         this.name = name;
     }
 
-    public boolean play() {
-        Random random = new Random();
-        int number = random.nextInt(10) + 1;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Gib eine Zahl zwischen 1 - 10 ein: ");
-        int guess = scanner.nextInt();
-
-        if (guess > 10 || guess < 0) {
-            System.out.println("Verloren...die Zahl liegt ausserhalb der Range");
-            return false;
-        } else if ((guess - number) < 2 && (guess - number) > -2) {
-            System.out.println("Gewonnen! Die Zahl war: " + number);
-            return true;
-        } else {
-            System.out.println("Verloren! Die Zahl war: " + number);
-            return false;
-        }
-    }
+    public abstract boolean play();
 
     public int payWin(int pot) {
         return pot * 2;
